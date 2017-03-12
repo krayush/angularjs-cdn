441af671163ec85930a9b103d96a40408e03ceac
Ayush Walia <ayush.walia.js@snapdeal.com>
2017-03-12 17:36:49 +0530

    Adding bootstrap + Sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
     moduleId: module.id,
     selector: 'events-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
-    constructor(router: Router) {
-        // console.error(router.config);
-    }
     title:string = 'Events App';
 }
